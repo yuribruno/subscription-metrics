5e128a0cb3ea6818b0c8fe7dd4403d456d1be67a
yuribruno <yuribruno@msn.com>
2024-01-08 00:15:20 -0300

    docker configuration

---
+++ app/src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/',
+      name: 'dashboard',
+      component: import('../views/DashboardView.vue'),
+      meta: {
+        auth: true
+      }
+    },
+  ]
+})
+
+export default router
---
+++ app/src/views/DashboardView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <v-app>
+      <v-navigation-drawer permanent>
+        <v-list-item title="My Application" subtitle="Vuetify"></v-list-item>
+        <v-divider></v-divider>
+        <v-list-item link title="List Item 1"></v-list-item>
+        <v-list-item link title="List Item 2"></v-list-item>
+        <v-list-item link title="List Item 3"></v-list-item>
+      </v-navigation-drawer>
+    </v-app>
+  </main>
+</template>
